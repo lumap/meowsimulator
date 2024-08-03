6b5bc99f07c16cc1b55733eac8c6f1fffdeb8dc5
lumap <lumap@duck.com>
2024-08-03 02:33:03 +0200

    modal: add classname and comments

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -28,29 +28,33 @@ A simple modal that can display any HTML you throw at it. Comes with animations 
 
 <script lang="ts">
     export let showModal: boolean;
+    
+    let className = "";
+    export { className as class };
+    
 
-    let dialog: HTMLDialogElement;
+    let dialog: HTMLDialogElement; // Reference to the dialog element. We'll execute functions on it.
     let closing = false; // Used to add animations for modal close
 
     const escToClose = (e: KeyboardEvent) => {
         if (e.key === "Escape") {
-            e.preventDefault();
+            e.preventDefault(); // By default, it insta closes the dialog. We don't want that.
             showModal = false;
         }
     };
 
     $: if (dialog) {
         if (showModal) {
+            // Open the dialog with the -in animations
             dialog.showModal();
             window.addEventListener("keydown", escToClose);
         } else {
-            closing = true;
+            closing = true; // Swap to the --out animations and start them
             window.removeEventListener("keydown", escToClose);
             setTimeout(() => {
-                closing = false;
-                showModal = false;
-                dialog.close();
-            }, 200);
+                closing = false; // Reset the animations, in case we need to open the modal again
+                dialog.close(); // Actually close it
+            }, 200); // 200ms delay set in ./tailwind.config.ts
         }
     }
 </script>
@@ -58,6 +62,7 @@ A simple modal that can display any HTML you throw at it. Comes with animations 
 <dialog
     bind:this={dialog}
     class="p-8 rounded-md"
+    class:className
     class:animate-zoom-in={!closing}
     class:animate-zoom-out={closing}
     class:backdrop:animate-fade-in={!closing}
